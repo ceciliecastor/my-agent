9fcb218a8f0b54900a21576070dbfbf4067a33e3
ceciliecastor <ceciliecastor@gmail.com>
2023-05-09 11:31:54 +0200

    Begyndt at lave home-card Begyndt på homepage Begyndt at linke til detaljeside med params

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,80 @@
 import GetAllHomes from "@/lib/GetAllHomes";
+import Link from "next/link";
 
 export default async function homePage() {
   const homesData: Promise<Homes[]> = GetAllHomes();
   const homes = await homesData;
 
   const content = (
-    <article>
-      <section>
-        <h2 className="text-white">Søg efter din drømme bolig</h2>
+    <article className="grid content-center justify-center">
+      <article className="grid place-content-center">
+        <img src="" alt="" />
+        <h2 className="text-black">Søg efter din drømme bolig</h2>
         <section>
           <h4>Søg blandt 158 boliger til salg i 74 butikker</h4>
           <p>Hvad skal din næste bolig indeholde?</p>
           <input type="text" />
         </section>
-      </section>
+      </article>
+      <article className="max-w-[1108px]">
+        <section>
+          <img src="" alt="" />
+          <div>
+            <h3>Vi har fulgt danskerne hjem i snart 4 årtier</h3>
+            <h4>De synes vi siger noget om os!</h4>
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has normal distribution.
+            </p>
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout.
+            </p>
+          </div>
+        </section>
+      </article>
+      <article>
+        <h3>Udvalgte boliger</h3>
+        <p>
+          There are many variations of passages of Lorem Ipsum available but the
+          this in majority have suffered alteration in some
+        </p>
+        <section className="grid grid-cols-2 gap-[25px] max-w-[1108px]">
+          {homes.slice(0, 4).map((home) => (
+            <Link href={`/${home.id}`}>
+              <div
+                className=" bg-white rounded-lg shadow dark:bg-white dark:border-gray-700"
+                key={home.id}
+              >
+                <img
+                  className="w-full rounded-t-lg"
+                  src={home.images[0].formats.thumbnail.url}
+                  alt={home.adress1}
+                />
+                <section className="p-25">
+                  <h4>{home.adress1}</h4>
+                  <div>
+                    <p>{home.postalcode}</p>
+                    <p>{home.city}</p>
+                  </div>
+                  <div>
+                    <h4>{home.type}</h4>
+                    <p>Ejerudgift {home.cost} kr.</p>
+                  </div>
+                  <div className="grid">
+                    <p>{home.energylabel}</p>
+                    <h4>{home.rooms} værelser</h4>
+                    <h4>{home.livingspace} m2</h4>
+                    <p>Kr. {home.price}</p>
+                  </div>
+                </section>
+              </div>
+            </Link>
+          ))}
+        </section>
+      </article>
     </article>
   );
+  return content;
 }
--- type.d.ts
+++ type.d.ts
@@ -1,51 +1,181 @@
+// type Homes = {
+//   energylabel: String;
+//   type: String;
+//   images: [];
+//   gross: Number;
+//   payment: Number;
+//   price: Number;
+//   city: String;
+//   cost: Number;
+//   basementsize: Number;
+//   lotsize: Number;
+//   netto: Number;
+//   rooms: String;
+//   adress1: String;
+//   livingspace: Number;
+//   built: Number;
+//   floorplan: [];
+//   description: String;
+//   lat: Number;
+//   long: Number;
+//   agent: [];
+//   users: [];
+//   id: String;
+// };
+
+// type Images = {
+//   name: String;
+//   size: number;
+//   width: number;
+//   height: number;
+//   url: string;
+//   formats: [];
+// };
+
+// type Formats = {
+//     name: String,
+//     hash: String,
+//     ext: String,
+//     mime: String,
+//     width: String,
+//     height: String,
+//     size: String,
+//     path: null,
+//     url: String,
+
+// }
+
+// type Floorplan = {
+//   name: String;
+//   size: Number;
+// };
+
+// type Agent = {
+//   name: String;
+//   title: String;
+//   phone: String;
+//   email: String;
+//   image: [];
+//   id: String;
+// };
+
+// type Image = {
+//   name: String;
+//   size: number;
+// };
+
 type Homes = {
-  energylabel: String;
-  type: String;
-  images: [];
-  gross: Number;
-  payment: Number;
-  price: Number;
-  city: String;
-  cost: Number;
-  basementsize: Number;
-  lotsize: Number;
-  netto: Number;
-  rooms: String;
-  adress1: String;
-  livingspace: Number;
-  built: Number;
-  floorplan: [];
-  description: String;
-  lat: Number;
-  long: Number;
-  agent: [];
+  energylabel: string;
+  type: string;
+  images: {
+    name: string;
+    size: number;
+    width: number;
+    height: number;
+    url: string;
+    formats: {
+      thumbnail: {
+        name: string;
+        hash: string;
+        ext: string;
+        mime: string;
+        width: number;
+        height: number;
+        size: number;
+        path: null;
+        url: string;
+        provider_metadata: {
+          public_id: string;
+          resource_type: string;
+        };
+      };
+    };
+    provider_metadata: {
+      public_id: string;
+      resource_type: string;
+      id: string;
+    };
+  }[];
+  gross: number;
+  payment: number;
+  price: number;
+  city: string;
+  cost: number;
+  basementsize: number;
+  lotsize: number;
+  netto: number;
+  postalcode: number;
+  rooms: string;
+  adress1: string;
+  livingspace: number;
+  built: number;
+  floorplan?: {
+    name: string;
+    size: number;
+    url: string;
+    formats: {
+      thumbnail: {
+        name: string;
+        hash: string;
+        ext: string;
+        mime: string;
+        width: number;
+        height: number;
+        size: number;
+        path: null;
+        url: string;
+        provider_metadata: {
+          public_id: string;
+          resource_type: string;
+        };
+      };
+    };
+    provider_metadata: {
+      public_id: string;
+      resource_type: string;
+      width: number;
+      height: number;
+      id: string;
+    };
+  };
+  description: string;
+  lat: number;
+  long: number;
+  agent: {
+    name: string;
+    title: string;
+    phone: string;
+    email: string;
+    image: {
+      name: string;
+      size: number;
+      url: string;
+      formats: {
+        thumbnail: {
+          name: string;
+          hash: string;
+          ext: string;
+          mime: string;
+          width: number;
+          height: number;
+          size: number;
+          path: null;
+          url: string;
+          provider_metadata: {
+            public_id: string;
+            resource_type: string;
+          };
+        };
+      };
+      provider_metadata: {
+        public_id: string;
+        resource_type: string;
+        width: number;
+        height: number;
+        id: string;
+      };
+    };
+  };
   users: [];
-  id: String;
-};
-
-type Images = {
-  name: String;
-  size: number;
-  width: number;
-  height: number;
-  url: string;
-};
-
-type Floorplan = {
-  name: String;
-  size: Number;
-};
-
-type Agent = {
-  name: String;
-  title: String;
-  phone: String;
-  email: String;
-  image: [];
-  id: String;
-};
-
-type Image = {
-  name: String;
-  size: number;
+  id: string;
 };
