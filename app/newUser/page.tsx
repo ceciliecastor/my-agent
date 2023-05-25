18af141a3feb84bb9300fea04bc3ffa62f5f6bc5
ceciliecastor <ceciliecastor@gmail.com>
2023-05-25 13:22:42 +0200

    Lavet funktionelt button component Lavet Icon component Begyndt på Prisma schema og mongoDB Begyndt på login funktionen Begyndt på energy label

---
+++ app/newUser/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Heading from "../component/Heading";
+
+export default function NewUser() {
+  return (
+    <article>
+      <Heading text="Account register" />
+    </article>
+  );
+}
